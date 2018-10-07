7e51ed65600ee05888d99cf585c648fdc3b84121
Abdul Rafi <arafi6624@bths.edu>
2018-10-07 16:01:04 -0400

    All required methods complete.

--- src/ARafiLib.java
+++ src/ARafiLib.java
@@ -1,70 +1,91 @@
 public class ARafiLib {
 
-    public static boolean isPalindrome(String str){
+    /**
+     * Checks whether a given string is a palindrome or not.
+     * Reverses the order of each letter in the string to see if it matches the original string.
+     *
+     * @param str a string given by the user.
+     * @return true or false based on the string being a palindrome or not.
+     */
+
+    public static boolean isPalindrome(String str) {
         int i = 0;
         int x = str.length();
         String result = str;
         String result2 = "";
         while (i < x) {
-            String str1 = result.substring(x - 1); // Getting the last character
+            String str1 = result.substring(x - 1);
             String str2 = result.substring(0, x - 1);
             result = str1 + str2;
             result2 = result2 + str1;
             i++;
         }
-        if (result2.equals(str)){
+        if (result2.equals(str)) {
             return true;
-        }
-        else {
+        } else {
             return false;
         }
     }
 
-    public static String cutOut(String mainStr, String subStr){
+    /**
+     * Takes out a given string from another given string.
+     *
+     * @param mainStr A string given by the user.
+     * @param subStr  A second string given by the user to take out from mainStr.
+     * @return The result of subStr taken out from mainStr.
+     */
+
+    public static String cutOut(String mainStr, String subStr) {
         int i = mainStr.indexOf(subStr);
-        String one = mainStr.substring(0,i);
+        String one = mainStr.substring(0, i);
         String two = mainStr.substring(i + subStr.length());
         return one + two;
     }
 
-    public static int sumUpTo(int n){
+    /**
+     * Adds all the integers preceding the given input n.
+     *
+     * @param n An integer value given by the user.
+     * @return An integer value that is the sum of all the numbers before the given integer n.
+     */
+
+    public static int sumUpTo(int n) {
         int result = 0;
-        for(int i = 0; i <= n; i++){
+        for (int i = 0; i <= n; i++) {
             result = result + i;
         }
         return result;
     }
 
-    public static String primePrinter(int num) {
-        String result = " ";
-        int i = 1;
-        while (i < num){
-            for (int a = 2; a < i; a++){
-                if(i % a == 0){
+    /**
+     * Reformats the given date from mm/dd/yyyy to dd-mm-yyyy.
+     *
+     * @param str An string input by the user in the format mm/dd/yyyy.
+     * @return A string in the format dd-mm-yyyy.
+     */
 
-                }
-            }
-            result = i + result;
-            i++;
-        }
-        return result;
-    }
-
-    public static String dateStr(String str){
-        String month = str.substring(0,2);
-        String date = str.substring(3,5);
+    public static String dateStr(String str) {
+        String month = str.substring(0, 2);
+        String date = str.substring(3, 5);
         String year = str.substring(6);
         return date + "-" + month + "-" + year;
     }
 
-    public static int leastCommonMultiple(int num1, int num2, int num3){
-        int i = 2;
-        while (i < num1){
-            if (num1%i == 0 && num2%i == 0 && num3%i == 0){
-                return i;
-            }
-            i++;
+    /**
+     * Prints a multiplication table with the product of the base * n (all integers from 0 to n).
+     *
+     * @param base  An integer input by the user.
+     * @param range An integer input by the user.
+     * @return A String of integers that are the product of the base * n.
+     */
+
+    public static String multiplicationTable(int base, int range) {
+        String result = "";
+        int a;
+        for (int n = 0; n <= range; n++) {
+            a = base * n;
+            result = result + a + " ";
         }
-        return i;
+        return result;
     }
 }
--- src/TestFile.java
+++ src/TestFile.java
@@ -1,10 +1,9 @@
 public class TestFile {
     public static void main(String[] args) {
-        System.out.println(ARafiLib.isPalindrome("racecar"));
-        System.out.println(ARafiLib.cutOut("word", "or"));
-        System.out.println(ARafiLib.sumUpTo(10));
-        System.out.println(ARafiLib.primePrinter(7));
-        System.out.println(ARafiLib.dateStr("09/22/2002"));
-        System.out.println(ARafiLib.leastCommonMultiple(4,4,5));
+        System.out.println("Is it a palindrome: " + ARafiLib.isPalindrome("ana"));
+        System.out.println("The cut-out word is: " + ARafiLib.cutOut("string", "ring"));
+        System.out.println("The sum off all integers from 0 to n is: " + ARafiLib.sumUpTo(4));
+        System.out.println("The reformatted date is: " + ARafiLib.dateStr("10/07/2018"));
+        System.out.println("All the products of base * (0 to range) is: " + ARafiLib.multiplicationTable(2,3));
     }
 }
